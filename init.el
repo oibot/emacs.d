3ce8af3df0fb97fccb88a30f86b0a54d1674362e
Tobias Ostner <tobias.ostner@gmail.com>
2017-01-12 15:37:04 +0100

    Refresh package contents at startup

--- init.el
+++ init.el
@@ -2,10 +2,13 @@
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives
-	     '("org" . "http://orgmode.org/elpa/") t)
+;; (add-to-list 'package-archives
+;; 	     '("org" . "http://orgmode.org/elpa/") t)
 
 (package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
